fc5e7599ee6b7a39e7738eafae3b941b4c77d4b8
Muhammad Syafrizal <muhammadsyafr@gmail.com>
2020-03-22 16:38:15 +0700

    firstcommit to my blog

---
+++ src/components/layout.js
@@ -0,0 +1,77 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import { rhythm, scale } from "../utils/typography"
+
+const Layout = ({ location, title, children }) => {
+  const rootPath = `${__PATH_PREFIX__}/`
+  let header
+
+  if (location.pathname === rootPath) {
+    header = (
+      <center>
+      <h1
+        style={{
+          ...scale(1),
+          marginBottom: rhythm(1.5),
+          marginTop: 0,
+          fontWeight: '800',
+          color: '#303952'
+        }}
+      >
+        <Link
+          style={{
+            boxShadow: `none`,
+            textDecoration: `none`,
+            color: `inherit`,
+          }}
+          to={`/`}
+        >
+          {title}
+        </Link>
+      </h1>
+      </center>
+    )
+  } else {
+    header = (
+      <h3
+        style={{
+          fontFamily: `Montserrat, sans-serif`,
+          marginTop: 0,
+          color: '#ea8685'
+        }}
+      >
+        <Link
+          style={{
+            boxShadow: `none`,
+            textDecoration: `none`,
+            color: `inherit`,
+          }}
+          to={`/`}
+        >
+          {title}
+        </Link>
+      </h3>
+    )
+  }
+  return (
+    <div
+      style={{
+        marginLeft: `auto`,
+        marginRight: `auto`,
+        maxWidth: rhythm(24),
+        padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
+      }}
+    >
+      <header>{header}</header>
+      <main>{children}</main>
+      <footer>
+        © {new Date().getFullYear()}, Built with
+        {` `}
+        <a href="https://www.gatsbyjs.org">Gatsby</a> with &hearts;
+      </footer>
+    </div>
+  )
+}
+
+export default Layout
